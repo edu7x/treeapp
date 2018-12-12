6672cb05126adc7e32d4405fd307ae7b8408228a
Nayra Sousa de Oliveira <nayra.sro@gmail.com>
2018-12-12 02:55:50 +0000

    Modulo Cadastro Especies

---
+++ src/Handler/Especies.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.Especies where
+
+import Import
+
+formEspecies :: Form Especies
+formEspecies = renderBootstrap $ Especies 
+    <$> areq textField "Nome Popular: " Nothing
+    <*> areq textField "Nome Científico: " Nothing
+
+getEspeciesCadastrarR :: Handler Html
+getEspeciesCadastrarR = do 
+    --gera formulario na widgetForm
+    (widgetForm, enctype) <- generateFormPost formEspecies
+    mensagem <- getMessage
+    defaultLayout $ do 
+        addStylesheet $ StaticR css_bootstrap_css
+        $(whamletFile "templates/especiesCadastrar.hamlet")    
+
+postEspeciesCadastrarR :: Handler Html 
+postEspeciesCadastrarR = do 
+    -- ler a informacao digitada
+    ((res,_),_) <- runFormPost formEspecies
+    case res of 
+        FormSuccess (especie) -> do
+            esp <- runDB $ selectFirst [EspeciesNomeCientifico ==. especiesNomeCientifico especie,
+                                            EspeciesNomePopular ==. especiesNomePopular especie] []
+            case esp of
+                Just _ -> do
+                    setMessage [shamlet|
+                        <h1>
+                            Especie já cadastrada!
+                    |]
+                    redirect EspeciesCadastrarR
+                Nothing -> do 
+                    _ <- runDB $ insert especie
+                    setMessage [shamlet|
+                        <h1>
+                            Especie cadastrada com sucesso!
+                    |]
+                    redirect EspeciesCadastrarR
+        _ -> redirect EspeciesCadastrarR
