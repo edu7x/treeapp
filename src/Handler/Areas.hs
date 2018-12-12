cd5c2061dd06de25dc6f3d3349f07d7d657ffeb5
Eduardo Botelho Junior <eduardo_botelho_7@hotmail.com>
2018-12-12 04:15:32 +0000

    Modulo Cadastrar Areas

---
+++ src/Handler/Areas.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.Areas where
+
+import Import
+
+formAreas :: Form Areas
+formAreas = renderBootstrap $ Areas
+    <$> areq textField "Descricao: " Nothing
+    <*> areq textField "Latitude 1: " Nothing
+    <*> areq textField "Longitude 1: " Nothing
+    <*> areq textField "Latitude 2: " Nothing
+    <*> areq textField "Longitude 2: " Nothing
+    <*> areq textField "Latitude 3: " Nothing
+    <*> areq textField "Longitude 3: " Nothing
+    
+getAreasCadastrarR :: Handler Html
+getAreasCadastrarR = do 
+    (widgetForm, enctype) <- generateFormPost formAreas
+    mensagem <- getMessage
+    defaultLayout $ do 
+        addStylesheet $ StaticR css_bootstrap_css
+        $(whamletFile "templates/areasCadastrar.hamlet") 
+
+postAreasCadastrarR :: Handler Html 
+postAreasCadastrarR = do 
+    ((res,_),_) <- runFormPost formAreas
+    case res of 
+        FormSuccess (area) -> do
+            are <- runDB $ selectFirst [AreasDescricao ==. areasDescricao area
+                                        ] []
+            case are of
+                Just _ -> do                            
+                    setMessage [shamlet|
+                        <h1>
+                            Area já cadastrada!
+                    |]
+                    redirect AreasCadastrarR
+                Nothing-> do 
+                    _ <- runDB $ insert area
+                    setMessage [shamlet|
+                        <h1>
+                            Area cadastrada com sucesso!
+                    |]
+                    redirect AreasCadastrarR
+        _ -> redirect AreasCadastrarR
